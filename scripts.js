1917fc386c0d2d6e6fe2a9357d13463e49c5d366
Mario Roberto <mrgomez.dev@gmail.com>
2024-03-26 13:10:59 -0600

    Creation dashboard basic

---
+++ scripts.js
@@ -0,0 +1,27 @@
+document.getElementById("opcion1").addEventListener("click", function() {
+    document.getElementById("contenido").innerHTML = "<h2>Contenido Opción 1</h2><p>Este es el contenido de la Opción 1.</p>";
+});
+
+document.getElementById("opcion2").addEventListener("click", function() {
+    document.getElementById("contenido").innerHTML = "<h2>Contenido Opción 2</h2><p>Este es el contenido de la Opción 2.</p>";
+});
+
+document.getElementById("opcion3").addEventListener("click", function() {
+    document.getElementById("contenido").innerHTML = "<h2>Contenido Opción 3</h2><p>Este es el contenido de la Opción 3.</p>";
+});
+
+document.getElementById("opcion4").addEventListener("click", function() {
+    document.getElementById("contenido").innerHTML = "<h2>Contenido Opción 4</h2><p>Este es el contenido de la Opción 4.</p>";
+});
+
+document.getElementById("opcion5").addEventListener("click", function() {
+    document.getElementById("contenido").innerHTML = "<h2>Contenido Opción 5</h2><p>Este es el contenido de la Opción 5.</p>";
+});
+
+document.getElementById("opcion6").addEventListener("click", function() {
+    document.getElementById("contenido").innerHTML = "<h2>Contenido Opción 6</h2><p>Este es el contenido de la Opción 6.</p>";
+});
+
+document.getElementById("opcion7").addEventListener("click", function() {
+    document.getElementById("contenido").innerHTML = "<h2>Contenido Opción 7</h2><p>Este es el contenido de la Opción 7.</p>";
+});
